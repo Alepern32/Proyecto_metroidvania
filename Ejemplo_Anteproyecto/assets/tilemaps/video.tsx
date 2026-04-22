6b3f85508ddcd83dad979b6c3b3a6c31a43d732d
Adrián Álvarez Bombín <adrialvarezbom@gmail.com>
2026-04-22 08:55:16 +0200

    Delete Ejemplo_Anteproyecto directory

--- Ejemplo_Anteproyecto/assets/tilemaps/video.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="video" tilewidth="32" tileheight="32" tilecount="80" columns="10">
- <image source="../../bosque.png" width="320" height="272"/>
-</tileset>
